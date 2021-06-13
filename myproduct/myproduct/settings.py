cce41af9761d07d164cc6d06bbfc5aceeecec469
Akash-Salvi <akashnov100@gmail.com>
2021-06-13 11:54:40 +0530

    [Host] Hosting

--- myproduct/myproduct/settings.py
+++ myproduct/myproduct/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -137,3 +139,10 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+
+
+
+
+
+django_heroku.settings(locals())
